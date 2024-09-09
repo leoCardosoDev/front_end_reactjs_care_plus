e541f268c4143a16146cbab4ccbc2c36eae20ff5
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-09 12:12:23 -0700

    feat: add layout and patient forms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
-const fontsSans = Plus_Jakarta_Sans({
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-sans",
@@ -12,7 +13,11 @@ const fontsSans = Plus_Jakarta_Sans({
 
 export const metadata: Metadata = {
   title: "CarePulse",
-  description: "A healthcare management system",
+  description:
+    "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
+  icons: {
+    icon: "/assets/icons/logo-icon.svg",
+  },
 };
 
 export default function RootLayout({
@@ -21,11 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-dark-300 font-sans, antialiased",
-          fontsSans.variable
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="dark">
